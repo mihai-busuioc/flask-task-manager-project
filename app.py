93b0f30eea1e8e843994b3267b9b3e0a7c86bd75
Mihai Busuioc <mihai.busuioc@freshfairy.ro>
2021-01-19 17:31:06 +0000

    added login  functionality

--- app.py
+++ app.py
@@ -44,7 +44,33 @@ def register():
         session["user"] = request.form.get("username").lower()
         flash("Registration Successfull")
     return render_template("register.html")
-    
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        #Check if the username exists in db
+        existing_user =mongo.db.users.find_one(
+            {"username":request.form.get("username".lower())})
+
+        if existing_user:
+            #ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("Incorect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+        # username doesn't exist
+            flash("Incorect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
 
 
 if __name__ == "__main__":
